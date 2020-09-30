1f6b5606585bc87bee6a3b2dfc1352df7d9b6e6c
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-09-30 19:20:16 -0300

    database config

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
 export const database = new Sequelize(
-    'equaledatabase',
-    'root',
-    'imaginie',
-    { dialect: 'mysql'}
+    '',
+    '',
+    '',
+    { dialect: ''}
 );
 
