c7935c78cd2d1629ee22c1eea0285e1cf77292eb
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-11-19 00:31:25 -0300

    deploy e migrations

--- sequelize-migrations/config/config.js
+++ sequelize-migrations/config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "imaginie",
-    "database": "equaledatabase",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST_NAME,
     "dialect": "mysql",
     "operatorsAliases": 0
   },
@@ -23,4 +25,4 @@
     "dialect": "mysql",
     "operatorsAliases": false
   }
-}
+};
