9c8d99d606d2c500105e8a435eae3706619490d0
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-11-24 00:33:04 -0300

    env migrations

--- sequelize-migrations/config/config.js
+++ sequelize-migrations/config/config.js
@@ -15,14 +15,14 @@ module.exports = {
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": 0
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST_NAME,
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": 0
   }
 };
