5edd8342bfc98bcce53021cd1aa8d526bf6f7bc0
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-11-27 23:51:51 -0300

    config database

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -2,9 +2,11 @@ import { Sequelize } from "sequelize";
 
 require('dotenv').config();
 
-export const database = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    { dialect: 'mysql'}
-);
+export const database = process.env.CLEARDB_DATABASE_URL ?
+    new Sequelize(process.env.CLEARDB_DATABASE_URL) :
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        { dialect: 'mysql'}
+    );
